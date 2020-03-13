4d617b93afd2e1553480f596c6ca95a025baa6cc
Dat Lam <tlam@samhammer.de>
2020-03-13 20:01:33 +0700

    ASMO-1498: Port project to netcore3.1.1

---
+++ src/Samhammer.Web.Common/Http/UnsignedHttpClientBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Samhammer.Web.Common.Http
+{
+    public class UnsignedHttpClientBuilder
+    {
+        public static void Resolve(IServiceCollection serviceCollection)
+        {
+            // Generic client
+            serviceCollection.AddHttpClient();
+
+            AddUnsignedHttpClient(serviceCollection);
+        }
+
+        private static void AddUnsignedHttpClient(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<UnsignedHttpClientHandler>();
+
+            serviceCollection
+                .AddHttpClient(HttpClientNames.UnsignedHttpClient)
+                .ConfigurePrimaryHttpMessageHandler(sp => sp.GetService<UnsignedHttpClientHandler>());
+        }
+    }
+}
