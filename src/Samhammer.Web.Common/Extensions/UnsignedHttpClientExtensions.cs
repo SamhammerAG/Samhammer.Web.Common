66392c71b0aa6d79e769f1bbe37c76298a0ebb08
Roland Schmitt <rschmitt@samhammer.de>
2022-01-19 14:15:53 +0100

    ASMO-3956 remove obsolete stuff #1

--- src/Samhammer.Web.Common/Extensions/UnsignedHttpClientExtensions.cs
+++ src/Samhammer.Web.Common/Extensions/UnsignedHttpClientExtensions.cs
@@ -1,16 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Samhammer.Web.Common.Http;
 
 namespace Samhammer.Web.Common.Extensions
 {
     public static class UnsignedHttpClientExtensions
     {
-        public static void AddUnsignedHttpClient(this IWebHostBuilder webHostBuilder)
-        {
-            webHostBuilder.ConfigureServices(AddUnsignedHttpClient);
-        }
-
         public static void AddUnsignedHttpClient(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<UnsignedHttpClientHandler>();
